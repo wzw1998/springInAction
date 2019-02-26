1fb995e155ead2993a95643200eb713347138990
Joson <1010761538@qq.com>
2019-02-26 18:13:03 +0800

    创建spring web应用程序

---
+++ Chapter_05/src/main/java/com/zexing/spittr/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.zexing.spittr.web;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//声明一个控制器
+@RequestMapping("/")
+public class HomeController {
+
+  @RequestMapping(method = GET)//处理get请求
+  public String home(Model model) {
+    return "home";
+  }
+
+}
