40a22c6c69378d7480cb0ad4609cf5c328899eb1
zhangzexing789 <1010761538@qq.com>
2019-03-03 22:04:04 +0800

    构建spring web应用程序

--- Chapter_05/src/main/java/com/zexing/spittr/web/SpitterController.java
+++ Chapter_05/src/main/java/com/zexing/spittr/web/SpitterController.java
@@ -5,15 +5,15 @@ import com.zexing.spittr.repository.SpitterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
-import org.springframework.validation.Errors;
 
 @Controller
-@RequestMapping("/spittler")
+@RequestMapping("/spitter")
 public class SpitterController {
     /**
      *
